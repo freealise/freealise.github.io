a04577cd901a39ffc8f19123d69f906d628e0289
Marina Goritskaia <me@freealise.com>
2018-12-12 00:04:15 +0300

    Update giveaway.js

--- assets/js/giveaway.js
+++ assets/js/giveaway.js
@@ -14,14 +14,13 @@ function getSum() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      var row = JSON.parse(this.responseText).rows[0];
-      var sum = row[0];
-      document.getElementById('amountGiven').innerText='£'+sum;
+      var sum = this.responseText;
+      document.getElementById('amountGiven').innerText=sum;
       var amountNeeded = document.getElementById('amountNeeded').innerText.slice(1);
       document.getElementById('giveBar').style.width=sum/amountNeeded*100+'%';
     }
   };
-  xhttp.open("GET", "https://www.googleapis.com/fusiontables/v2/query?sql=SELECT%20SUM(%27Payment%20in%20£%27)%20FROM%2016IeDVvEj8g0xtHevc3QmvL3OO79kPRCfZ3ZoftAP%20WHERE%20'Payment%20in%20£'%20>0&key=AIzaSyAm9yWCV7JPCTHCJut8whOjARd7pwROFDQ", true);
+  xhttp.open("GET", "https://script.google.com/macros/s/AKfycby9VN3ghF35t1Z4csV9kycvQyBqiYF8irms_ybLMmsZrKFlU9rb/exec?a=balance", true);
   xhttp.send();
 }
 
@@ -41,6 +40,6 @@ function queue() {
       document.getElementById("queue").innerHTML = queue;
     }
   };
-  xhttp.open("GET", "https://www.googleapis.com/fusiontables/v2/query?sql=SELECT%20*%20FROM%2016IeDVvEj8g0xtHevc3QmvL3OO79kPRCfZ3ZoftAP%20WHERE%20'Payment%20in%20£'%20>0&key=AIzaSyAm9yWCV7JPCTHCJut8whOjARd7pwROFDQ", true);
+  xhttp.open("GET", "https://script.google.com/macros/s/AKfycby9VN3ghF35t1Z4csV9kycvQyBqiYF8irms_ybLMmsZrKFlU9rb/exec?a=queue", true);
   xhttp.send();
 }
