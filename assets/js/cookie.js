5afa3d28e34fe7d713713b9a7f6f050178440ad6
Marina Goritskaia <me@freealise.com>
2018-12-20 00:14:39 +0300

    Update cookie.js

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -6,15 +6,19 @@ function setCookie(cname,cvalue,exdays) {
 }
 
 function getCookie(cname) {
-    var decodedCookie = document.cookie;
-    var ca = decodedCookie.replace("; ", ";").split(";");
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i].split("=");
-	if (c[1] && c[0]==cname) {
-	  return c[1];
-	}
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
 
 var email='0';
