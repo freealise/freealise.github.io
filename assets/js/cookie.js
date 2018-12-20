215e171eb89d2b937ad18391a01034dbeb48fc38
Marina Goritskaia <me@freealise.com>
2018-12-20 15:05:28 +0300

    Update cookie.js

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -19,34 +19,3 @@ function getCookie(cname) {
   }
   return "";
 }
-
-var email='0';
-var token='0';
-var url = window.location.href.split("?");
-if (url[1]) {
-  var params = url[1].split("&");
-  if (params) {
-    for (var i=0; i<params.length; i++) {
-      if (params[i].split("=")[0] == "email") {email = params[i].split("=")[1];}
-      if (params[i].split("=")[0] == "token") {token = params[i].split("=")[1];}
-    }
-  }
-  if (email != '0' && token != '0') {
-    if (email != "" && token != "") {
-      setCookie("email", email, 365);
-      setCookie("token", token, 365);
-      
-	  document.getElementById('email').value=email;
-	  document.getElementById('token').value=token;
-    } else if (email == "" && token == "") {
-      setCookie("email", "", -1);
-      setCookie("token", "", -1);
-      
-	  document.getElementById('email').value='';
-	  document.getElementById('token').value='';
-    }
-  }
-}
-
-var email=getCookie("email");
-var token=getCookie("token");
