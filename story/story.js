8eab2b1a20d09cd60079e1349f33b09dbe42d7aa
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2021-07-26 21:52:11 +0300

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,6 +1,22 @@
 var json = {
-  firstName : "John",
-  lastName  : "Doe",
-  age     	: 50,
-  eyeColor  : "blue"
+  setUp: { 
+    des: "You are looking out the window. It still doesn't rain.",
+    act: ["curse", "pray", "sleep", "work"],
+    res: [
+      "Nothing happens. You don't even feel better.",
+      "You want to, but find no appropriate words.",
+      "Take a nap for a couple of hours. Nothing changes.",
+      catalyst,
+    ],
+  },
+  catalyst: { 
+    des: "You hear quiet rapping against the window.",
+    act: ["", "", "", ""],
+    res: [
+      "",
+      "",
+      "",
+      debate,
+    ],
+  },
 };
