9369436b15b3a539ea61ec9720a95942a2095a11
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2021-10-10 23:26:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-        function subscribeEmail(address) {
-          var xhttp = new XMLHttpRequest();
-          xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-              document.getElementById('subscribe-form').innerHTML =
-              this.responseText;
-            }
-          };
-          xhttp.open('GET', 'https://script.google.com/macros/s/AKfycbz5br4wnfSGtucWKwGQq1Tb07eshJez6uVaFatn4xJAc_rcrcA/exec?a=subscribe&email='+address, true);
-          xhttp.send();
+        function subscribeEmail(address, un) {
+          var at = address.indexOf('@');
+          var dot = address.indexOf('.')+1;
+          if (at > 0 && dot > at && address.length > dot) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+              if (this.readyState == 4 && this.status == 200) {
+                document.getElementById('subscribe-form').innerHTML =
+                this.responseText;
+              }
+            };
+            xhttp.open('GET', 'https://script.google.com/macros/s/AKfycbz5br4wnfSGtucWKwGQq1Tb07eshJez6uVaFatn4xJAc_rcrcA/exec?a='+un+'subscribe&email='+address, true);
+            xhttp.send();
+          }
         }
