599e72cffc0093526c6b75c246c8e9e0db458740
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2021-10-20 21:25:49 +0300

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -19,7 +19,9 @@ function subscribeEmail(address, s) {
 
 function sendEmail() {
   var _name = document.getElementById("contact-form-name").value;
+  var _site = document.getElementById("contact-form-site").value;
   var _email = document.getElementById("contact-form-email").value;
+  var _subject = document.getElementById("contact-form-subject").value;
   var _message = document.getElementById("contact-form-email-message").value;
   var at = _email.indexOf('@');
   var dot = _email.indexOf('.')+1;
@@ -27,15 +29,10 @@ function sendEmail() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        var _response = JSON.parse(this.responseText);
-        if (_response.details.emailSentStatus == "true") {
-          document.getElementById("contact-form-sent").innerHTML = "Your message has been sent";
-        } else {
-          document.getElementById("contact-form-sent").innerHTML = "Could not send your message because of error:<br/>" + this.responseText;
-        }
+        document.getElementById("contact-form-sent").innerHTML = this.responseText;
       }
     };
-    xhttp.open("POST", "https://www.blogger.com/contact-form.do?name="+encodeURIComponent(_name)+"&email="+encodeURIComponent(_email)+"&message="+encodeURIComponent(_message)+"&blogID=5409931531965752101", true);
+    xhttp.open('GET', 'https://script.google.com/macros/s/AKfycbz5br4wnfSGtucWKwGQq1Tb07eshJez6uVaFatn4xJAc_rcrcA/exec?a=contact&name='+encodeURIComponent(_name)+'&site='+encodeURIComponent(_site)+'&email='+encodeURIComponent(_email)+'&subj='+encodeURIComponent(_subject)+'&msg='+encodeURIComponent(_message), true);
     xhttp.send();
   }
 }
