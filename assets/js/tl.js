a399cf9d6ee53340d969e28cae5fda3b1cbae213
Marina <5275747+freealise@users.noreply.github.com>
2021-11-12 00:22:10 +0300

    Update tl.js

--- assets/js/tl.js
+++ assets/js/tl.js
@@ -9,8 +9,8 @@ var json = {
       "credit": ""
     },
     "text": {
-      "headline": "Notes",
-      "text": "<p>This blog is for intuitive musical and voice notation, which is more like MIDI sequencing than the usual score sheets, and doesn't require special literacy. It's actually simplified audio spectrograms.</p>"
+      "headline": "Songs",
+      "text": "<p>in intuitive musical and voice notation, which is more like MIDI sequencing than the usual score sheets, and doesn't require special literacy. It's actually simplified audio spectrograms.</p>"
     }
   },
   "events": [
